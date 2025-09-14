200e844d099800f4c521664d1fb31ac6cc2b2f73
arsiccana <ana.arsic27@gmail.com>
2025-09-14 13:45:10 +0200

    Authorization

--- src/main/java/com/urosdragojevic/realbookstore/controller/PersonsController.java
+++ src/main/java/com/urosdragojevic/realbookstore/controller/PersonsController.java
@@ -13,7 +13,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import jakarta.servlet.http.HttpSession;
-
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.context.SecurityContextHolder;
 import java.sql.SQLException;
 import java.util.List;
 import java.nio.file.AccessDeniedException;
@@ -33,6 +35,7 @@ public class PersonsController {
     }
 
     @GetMapping("/persons/{id}")
+    @PreAuthorize("hasAuthority('VIEW_PERSON')")
     public String person(@PathVariable int id, Model model, HttpSession session) {
         String csrf = session.getAttribute("CSRF_TOKEN").toString();
         model.addAttribute("CSRF_TOKEN", session.getAttribute("CSRF_TOKEN"));
@@ -41,8 +44,10 @@ public class PersonsController {
     }
 
     @GetMapping("/myprofile")
-    public String self(Model model, Authentication authentication) {
+    @PreAuthorize("hasAuthority('VIEW_MY_PROFILE')")
+    public String self(Model model, Authentication authentication, HttpSession session) {
         User user = (User) authentication.getPrincipal();
+        model.addAttribute("CSRF_TOKEN", session.getAttribute("CSRF_TOKEN"));
         model.addAttribute("person", personRepository.get("" + user.getId()));
         return "person";
     }
@@ -56,16 +61,34 @@ public class PersonsController {
     }
 
     @PostMapping("/update-person")
-    public String updatePerson(Person person, HttpSession session, @RequestParam("csrfToken") String csrfToken) throws AccessDeniedException {
+    public String updatePerson(Person person, HttpSession session, @RequestParam("csrfToken") String csrfToken, HttpServletRequest request) throws AccessDeniedException {
         String csrf = session.getAttribute("CSRF_TOKEN").toString();
         if(!csrf.equals(csrfToken)){
             throw new AccessDeniedException("Forbidden");
         }
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        User user = (User) authentication.getPrincipal();
+        boolean hasEditPermission = authentication.getAuthorities().stream()
+                .anyMatch(grantedAuthority -> grantedAuthority.getAuthority().equals("UPDATE_PERSON"));
+        boolean isSelfUpdate = false;
+
+        int personId = Integer.parseInt(person.getId());
+        isSelfUpdate = user.getId() == personId;
+
+        if (!hasEditPermission && !isSelfUpdate) {
+            throw new AccessDeniedException("Can`t update person");
+        }
         personRepository.update(person);
-        return "redirect:/persons/" + person.getId();
+        String referer = request.getHeader("Referer");
+        if (referer != null && referer.contains("/myprofile")) {
+            return "redirect:/myprofile";
+        } else {
+            return "redirect:/persons/" + person.getId();
+        }
     }
 
     @GetMapping("/persons")
+    @PreAuthorize("hasAuthority('VIEW_PERSONS_LIST')")
     public String persons(Model model) {
         model.addAttribute("persons", personRepository.getAll());
         return "persons";
